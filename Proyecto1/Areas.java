05b4e0e5001689668f40895ef65e3f062e3b714f
Jorge Ivan <jorgeperez@ciencias.unam.mx>
2020-04-20 00:18:35 -0500

    Se agregaron las clases de los usuarios y las interfaces

---
+++ Proyecto1/Areas.java
@@ -0,0 +1,3 @@
+public interface Areas{
+  public void getDatosAreas();
+}
--- Proyecto1/Main.java
+++ Proyecto1/Main.java
@@ -1,26 +1,24 @@
 public class Main{
 
   public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
-		int opcion;
+    int opcion;
 
-		do{
-			System.out.println("Hola Bienvenido selecciona una de las siguientes opciones dependiendo de tu categoría\n\n" +
-				"1.- Administrador\n" +
-				"2.- Profesor\n" +
-				"3.- Alumno\n" +
-				"0.- Salir\n");
+    do{
+      System.out.println("Hola Bienvenido selecciona una de las siguientes opciones dependiendo de tu categoría\n\n" +
+      "1.- Administrador\n" +
+      "2.- Profesor\n" +
+      "3.- Alumno\n" +
+      "0.- Salir\n");
 
-				String opcionUsuario = sc.nextLine();
+      String opcionUsuario = sc.nextLine();
+      opcion = Integer.parseInt(opcionUsuario);
 
-				opcion = Integer.parseInt(opcionUsuario);
-
-				switch(opcion){
-					case 1:
+      switch(opcion){
+        case 1:
         System.out.println("Hola Administrador que deseas hacer...:\n\n"+
         "1.- Consultar lista de alumnos inscritos en la escuela.\n"+
-        "2.- Consultar lista de alumnos de cada área.\n"+
+        "2.- Consultar lista de alumnos de cada area.\n"+
         "3.- Consultar lista de alumnos de opciones técnicas.\n"+
         "4.- Consultar lista de profesores.\n"+
         "5.- Graduar un alumno.\n"+
@@ -28,29 +26,24 @@ public class Main{
         "7.- Dar de baja un alumno.\n"+
         "8.- Contratar un profesor.\n"+
         "9.- Despedir un profesor.\n");
-
-						break;
-
-					case 2:
-
-						break;
-
-					case 3:
-
-
-						consola.ejecutarOrdenamiento(arreglo);
-						break;
-
-					case 0:
-						break;
-
-					default:
-						System.out.println("Elige una opcion valida.\n");
-						break;
-
-				}
-
-		}while(opcion != 0);
-	}
-
+        break;
+        case 2:
+        System.out.println("Hola Profesor que deseas hacer...:\n\n"+
+        "1.- Consultar información de un grupo.\n"+
+        "2.- Asignar calificacion a los alumnos.\n");
+        break;
+        case 3:
+        System.out.println("Hola Alumno que deseas hacer...:\n\n"+
+        "1.- Consultar calificacion de una materia.\n"+
+        "2.- Consultar promedio.\n"+
+        "3.- Inscribirse a una opcion tecnica.\n");
+        break;
+        case 0:
+        break;
+        default:
+        System.out.println("Elige una opcion valida.\n");
+        break;
+      }
+    }while(opcion != 0);
+  }
 }
